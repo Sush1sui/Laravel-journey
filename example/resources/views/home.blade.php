3207da01b733fad44147600015e35cc2db948471
Sush1sui <jplovesraw@gmail.com>
2024-10-07 10:41:46 +0800

    make pretty layout using tailwindcss

--- example/resources/views/home.blade.php
+++ example/resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
-    <!-- content -->
+    <x-slot:heading>
+        Homepage
+    </x-slot:heading>
+
+    <!-- content aka the default $slot -->
     <h1>Hello from the Homepage.</h1>
 </x-layout>
