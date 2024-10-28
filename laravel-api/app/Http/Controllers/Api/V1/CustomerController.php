e0739e6f063cf8fe7b44ea3ece9306e2dc96d61c
Sush1sui <jplovesraw@gmail.com>
2024-10-28 19:52:53 +0800

    updating with put and patch

--- laravel-api/app/Http/Controllers/Api/V1/CustomerController.php
+++ laravel-api/app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Filters\V1\CustomersFilter;
 use App\Models\Customer;
 use App\Http\Requests\V1\StoreCustomerRequest;
-use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Requests\V1\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
@@ -59,7 +59,9 @@ class CustomerController extends Controller
      */
     public function update(UpdateCustomerRequest $request, Customer $customer)
     {
-        //
+        $customer->update($request->all());
+
+        return response()->json(['message' => 'Customer updated successfully!', 'customer' => $customer]);
     }
 
     /**
